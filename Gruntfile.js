86f700e0b6e26e688aa4a30b5c7e5e4635179d76
Olga Popova <olpop992@gmail.com>
2018-05-29 14:41:38 +0500

    настроена автоматизация сборки

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,105 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks("grunt-contrib-less");
+    require("load-grunt-tasks")(grunt);
 
     grunt.initConfig({
+        copy: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: [
+                        "fonts/**/*.{woff,woff2}",
+                        "img/**",
+                        "js/**",
+                        ".html"
+                    ],
+                    dest: "build"
+                }]
+            },
+            html: {
+                files: [{
+                    expand: true,
+                    src: ["*.html"],
+                    dest: "build"
+                }]
+            }
+        },
+        clean: {
+            build: ["build"]
+        },
         less: {
             style: {
                 files: {
-                    "css/style.css": "less/style.less"
+                    "build/css/style.css": "less/style.less"
+                }
+            }
+        },
+        postcss: {
+            options: {
+                processors: [
+                    require("autoprefixer")({
+                        browsers: "last 2 versions"
+                    }), 
+                    require("css-mqpacker")({
+                        sort: true
+                    })
+                ]
+            },
+            dist: {
+              src: "build/css/*.css"
+            }
+        },
+        csso: {
+            style: {
+                options: {
+                    report: "gzip"
+                },
+                files: {
+                    "build/css/style.min.css": ["build/css/style.css"]
+                }
+            }
+        },
+        imagemin: {
+            images: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    src: ["build/img/**/*.{png,jpg}"]
+                }]
+            }
+        },
+        watch: {
+            html: {
+                files: ["*.html"],
+                tasks: ["copy: html"]
+            },
+            style: {
+                files: ["less/**/*.less"],
+                tasks: ["less", "postcss", "csso"]
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ["build/*.html", "build/css/*.css"]
+                },
+                options: {
+                    watchTask: true,
+                    server: "build"
                 }
             }
         }
     });
+
+    grunt.registerTask('serve', ['browserSync', 'watch']);
+    grunt.registerTask("build", [
+        "clean",
+        "copy",
+        "less",
+        "postcss",
+        "csso",
+        "imagemin"
+    ]);
 
 }
